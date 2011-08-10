3e8fe26f5c479e96a2807ade3d277d1b4dc1c19d
Jake Moffatt <jakeonrails@gmail.com>
2011-08-10 16:42:59 -0700

    added size

--- models/ship.rb
+++ models/ship.rb
@@ -2,12 +2,13 @@ class Ship
 
   attr_reader :x, :y
 
-  def initialize(player, x, y)
+  def initialize(player, x, y, size)
     self.player = player
     self.x = x
     self.y = y
     self.orientation = orientation
     self.health = health
+    self.size = size
   end
 
   def vertical?
