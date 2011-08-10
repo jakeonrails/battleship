a131c019b93975eb12b28ae36c10be22ec4e95b7
Greg Hurrell <greg@causes.com>
2011-08-10 16:45:53 -0700

    Add Board#place_ship method and supporting methods

--- models/board.rb
+++ models/board.rb
@@ -10,7 +10,32 @@ class Board
     self.width = size
     initialize_cells
   end
-  
+
+  # Place the ship on the board.
+  #
+  # Returns true on success, false on failure.
+  def place_ship ship
+    if ship.vertical?
+      x_delta, y_delta = 1, 0 # traverse vertically
+    else
+      x_delta, y_delta = 0, 1 # traverse horizontally
+    end
+
+    x, y = ship.x, ship.y
+    ship.size.times do
+      if get_cell x, y
+        raise ArgumentError, "cannot place ship at #{x}, #{y}"
+      end
+      x += x_delta
+      y += y_delta
+    end
+
+    x, y = ship.x, ship.y
+    ship.size.times do
+      set_cell x, y, ship
+    end
+  end
+
 private
 
   attr_writer :cells
@@ -19,5 +44,37 @@ private
     self.cells = Array.new(height) do
       Array.new(width) { nil }
     end
+  end
+
+  # Gets the value currently stored at x, y
+  #
+  # Returns either:
+  #   - nil for empty cells
+  #   - :hit for hit ships
+  #   - :miss for missed ships
+  #   - a ship instance (for unhit ships)
+  def get_cell x, y
+    unless valid_cell?(x, y)
+      raise ArgumentError, "cell out of bounds at #{x}, #{x}"
+    end
+
+    cells[x][y]
+  end
+
+  # Stores the specified value at x, y
+  #
+  # See Board#get_cell for valid possible values.
+  def set_cell x, y, value
+    unless valid_cell?(x, y)
+      raise ArgumentError, "cell out of bounds at #{x}, #{x}"
+    end
+
+    cells[x][y] = value
+  end
+
+  # Returns true if the specified x, y coordinates fall
+  # within the board.
+  def valid_cell? x, y
+    x > 0 && x < width && y > 0 && y < height
   end
 end # class Board
