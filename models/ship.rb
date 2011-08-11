1dbc4798773f9b43d98fd3d364c191878030d8ad
Jake Moffatt <jakeonrails@gmail.com>
2011-08-10 17:37:17 -0700

    Added shoot function and alive? to ship

--- models/ship.rb
+++ models/ship.rb
@@ -2,24 +2,38 @@ class Ship
 
   attr_accessor :x, :y, :size
   attr_accessor :orientation
+  attr_reader :board
+  attr_reader :health
 
-  def initialize(player, x, y, size)
-    self.player = player
+  def initialize(board, x, y, size, orientation)
+    self.board = board
     self.x = x
     self.y = y
     self.orientation = orientation
-    self.health = health
     self.size = size
+    self.health = " " * size
   end
 
   def vertical?
-    self.orientation == "vertical"
+    self.orientation == :vertical
+  end
+
+  def alive?
+    self.health.include?(" ")
+  end
+
+  def shoot(x, y)
+    if (self.orientation == :vertical)
+      index = y - self.y
+      self.health[index] = 'x'
+    else # horizontal
+      index = x - self.x
+      self.health[index] = "x"
+    end
   end
 
 private
-
-  attr_writer :player
-
+  attr_writer :board, :health
 
 end
 
---
+++ spec/ship_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper.rb"
+
+require "ship"
+require "board"
+
+describe Ship do
+
+  before :each do
+    @board = Board.new(10)
+  end
+
+  describe "#shoot" do
+    it "should set health when hit" do
+      @ship = Ship.new(@board, 5, 5, 5, :vertical)
+      @ship.shoot(5, 5)
+      @ship.health.should == "x    "
+      @ship.shoot(5, 0)
+      @ship.health.should == "x    "
+      @ship.shoot(5, 6)
+      @ship.health.should == "xx   "
+
+      @ship = Ship.new(@board, 5, 5, 5, :horizontal)
+      @ship.shoot(5, 5)
+      @ship.health.should == "x    "
+      @ship.shoot(0, 0)
+      @ship.health.should == "x    "
+      @ship.shoot(5, 0)
+      @ship.health.should == "x    "
+      @ship.shoot(6, 5)
+      @ship.health.should == "xx   "
+    end
+  end
+
+  describe "#alive?" do
+    it "should return false when ship is dead" do
+      @ship = Ship.new(@board, 0, 0, 1, :vertical)
+      @ship.alive?.should == true
+      @ship.shoot(0, 0)
+      @ship.alive?.should == false
+    end
+  end
+
+end
+
