b0853e8ce0a4d3e6087aa655c335b8e4b1d76f0b
Greg Hurrell <greg@causes.com>
2011-08-10 17:06:40 -0700

    Add Board#shoot method

--- models/board.rb
+++ models/board.rb
@@ -4,6 +4,7 @@ class Board
 
   class CellOccupied < ArgumentError; end
   class CellOutOfBounds < ArgumentError; end
+  class DuplicateShot < ArgumentError; end
 
   def initialize size
     if !size.is_a?(Fixnum) || size <= 0
@@ -37,6 +38,16 @@ class Board
       x += x_delta
       y += y_delta
     end
+  end
+
+  def shoot x, y
+    ship = get_cell x, y
+    unless ship.nil?
+      raise DuplicateShot, "cell #{x}, #{y} has already been shot"
+    end
+
+    ship.shoot(x, y) # let the ship know that it's been hit
+    set_cell(x, y, ship.alive? ? :hit : :sunk)
   end
 
 private
