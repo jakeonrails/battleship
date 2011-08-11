3ba2972858d445b87c7b9fef64eedea432a1c272
Jake Moffatt <jakeonrails@gmail.com>
2011-08-10 17:51:18 -0700

    More player stuff

--- models/player.rb
+++ models/player.rb
@@ -1,12 +1,32 @@
 class Player
 
-  def initialize(game, board, ships)
-    @board = Board.new
-    @ships = Array.new(game.ships_allowed)
+  attr_reader :board, :ships
+
+  def initialize(game, num_ships)
+    self.board = Board.new
+    self.ships = Array.new(num_ships)
   end
 
   def place_ships
+    self.ships.map! do |ship|
+      ship = Ship.new
+      do
+        success = randomly_place_ship(ship)
+      until success
+      ship
+    end
+  end
 
+  def randomly_place_ship(ship)
+    begin
+      ship.x = rand(board.width)
+      ship.y = rand(board.height)
+      ship.orientation = "vertical"
+      self.board.place_ship(ship)
+    rescue
+      return false
+    end
+    true
   end
 
 end
