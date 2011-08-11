0d36e2779c9d61fe44a049adadbde9fb205412d2
Jake Moffatt <jakeonrails@gmail.com>
2011-08-10 17:45:30 -0700

    Added play_turn and setup_boards and play

--- models/game.rb
+++ models/game.rb
@@ -1,19 +1,45 @@
 class Game
 
-  attr_accessor players
-
-
+  attr_accessor :players
 
   def initialize()
-
+    @players = []
+    @players << Player.new("jake")
+    @players << Player.new("greg")
   end
 
-
-  def play_turn(player, x, y)
-
+  def setup_boards()
+    @players.each do |player|
+      player.setup_board
+    end
   end
 
-  def
+  def play_turn
+    @players.each do |player|
+      player.take_turn
+    end
+  end
+
+  def check_for_victory()
+  end
+
+  def check_for_hit()
+
+    hit but no win
+    hit with victory
+    sunk a ship
+    out of bounds
+    second attempt
+    missed
+  end
+
+  def play()
+    do
+      play_turn
+    until (check_for_victory())
+  end
 
 end
+
+Game.new.play()
 
